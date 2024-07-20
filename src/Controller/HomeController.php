5669c856bb21e6c579f8e68c1b64566223e659a9
devsandanayake <devinsandanayake49@gmail.com>
2024-07-21 00:58:16 +0530

    update

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'app_home')]
+    public function index(ManagerRegistry $doctrine): JsonResponse
+    {
+        $users = $doctrine->getRepository(User::class)->findAll();
+        return $this->json($users);
+    }
+}
